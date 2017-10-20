65ae3c298f6a5ca93c5881c78c20e90e34bb408e
Brian Akins <bakins@rsglab.com>
2017-10-20 07:08:27 -0400

    Cleanup fastcgi code a bit

--- proxy.go
+++ proxy.go
@@ -5,82 +5,18 @@ package proxy
 
 import (
 	"bytes"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"path/filepath"
 	"strings"
 
-	"github.com/golang/protobuf/proto"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/transport"
 )
-
-// Codec returns a proxying grpc.Codec with the default protobuf codec as parent.
-//
-// See CodecWithParent.
-func Codec() grpc.Codec {
-	return CodecWithParent(&protoCodec{})
-}
-
-// CodecWithParent returns a proxying grpc.Codec with a user provided codec as parent.
-//
-// This codec is *crucial* to the functioning of the proxy. It allows the proxy server to be oblivious
-// to the schema of the forwarded messages. It basically treats a gRPC message frame as raw bytes.
-// However, if the server handler, or the client caller are not proxy-internal functions it will fall back
-// to trying to decode the message using a fallback codec.
-func CodecWithParent(fallback grpc.Codec) grpc.Codec {
-	return &rawCodec{fallback}
-}
-
-type rawCodec struct {
-	parentCodec grpc.Codec
-}
-
-type frame struct {
-	payload []byte
-}
-
-func (c *rawCodec) Marshal(v interface{}) ([]byte, error) {
-	out, ok := v.(*frame)
-	if !ok {
-		return c.parentCodec.Marshal(v)
-	}
-	return out.payload, nil
-
-}
-
-func (c *rawCodec) Unmarshal(data []byte, v interface{}) error {
-	dst, ok := v.(*frame)
-	if !ok {
-		return c.parentCodec.Unmarshal(data, v)
-	}
-	dst.payload = data
-	return nil
-}
-
-func (c *rawCodec) String() string {
-	return fmt.Sprintf("proxy>%s", c.parentCodec.String())
-}
-
-// protoCodec is a Codec implementation with protobuf. It is the default rawCodec for gRPC.
-type protoCodec struct{}
-
-func (protoCodec) Marshal(v interface{}) ([]byte, error) {
-	return proto.Marshal(v.(proto.Message))
-}
-
-func (protoCodec) Unmarshal(data []byte, v interface{}) error {
-	return proto.Unmarshal(data, v.(proto.Message))
-}
-
-func (protoCodec) String() string {
-	return "proto"
-}
 
 func (s *Server) streamHandler(srv interface{}, stream grpc.ServerStream) error {
 	lowLevelServerStream, ok := transport.StreamFromContext(stream.Context())
@@ -134,7 +70,12 @@ func (s *Server) streamHandler(srv interface{}, stream grpc.ServerStream) error 
 	req.Header.Set("Host", host)
 	req.URL.Host = host
 
-	resp, err := s.client.request(req, s.entryFile, nil)
+	env := map[string]string{
+		"DOCUMENT_ROOT":   s.docRoot,
+		"SCRIPT_FILENAME": s.entryFile,
+	}
+
+	resp, err := s.fastcgiClientPool.request(req, env)
 
 	if err != nil {
 		return grpc.Errorf(codes.Internal, "fastcgi request failed: %s", err)
@@ -176,7 +117,7 @@ func (s *Server) auxPathHandle(path string, filename string) http.Handler {
 			"DOCUMENT_ROOT":   docroot,
 			"SCRIPT_FILENAME": filename,
 		}
-		resp, err := s.client.request(r, filename, env)
+		resp, err := s.fastcgiClientPool.request(r, env)
 
 		if err != nil {
 			http.Error(w, err.Error(), 500)
