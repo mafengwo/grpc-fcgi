b8cff3099124a92fb316b9efa1ae22ae730fbe65
jjw <654377598@qq.com>
2018-12-04 14:56:47 +0800

    update trace

--- grpc/stream.go
+++ grpc/stream.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"gitlab.mfwdev.com/service/grpc-fcgi/fcgi"
-	"gitlab.mfwdev.com/service/grpc-fcgi/log"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
@@ -15,32 +14,22 @@ import (
 	"time"
 )
 
-type streamHandler struct {
-	fcgiOptions *FcgiOptions
-	fcgiClient  *fcgi.Transport
-
-	timeout         time.Duration
-	reservedHeaders []string
-
-	logger *log.Logger
-}
-
-func (sh *streamHandler) handleStream(srv interface{}, stream grpc.ServerStream) error {
+func (p *Proxy) handleStream(srv interface{}, stream grpc.ServerStream) error {
 	reqid := getRequestId(stream)
 	req := &request{
 		requestID:    reqid,
 		requestTime:  time.Now(),
-		fcgiRounds: &fcgiRequestRounds{rounds:[]*fcgiRequestRound{}},
-		accessLogger: sh.logger.NewAccessLogger(),
-		errorLogger:  sh.logger.NewErrorLogger().With(zap.String("request_id", reqid)),
+		fcgiRounds:   &fcgiRequestRounds{rounds: []*fcgiRequestRound{}},
+		accessLogger: p.logger.NewAccessLogger(),
+		errorLogger:  p.logger.NewErrorLogger().With(zap.String("request_id", reqid)),
 	}
 	defer req.logAccess()
 
 	req.errorLogger.Debug("request received")
 
 	var cancelc context.CancelFunc
-	if sh.timeout > 0 {
-		req.ctx, cancelc = context.WithTimeout(stream.Context(), sh.timeout)
+	if p.opt.Timeout > 0 {
+		req.ctx, cancelc = context.WithTimeout(stream.Context(), time.Duration(p.opt.Timeout)*time.Second)
 	} else {
 		req.ctx, cancelc = context.WithCancel(stream.Context())
 	}
@@ -48,7 +37,7 @@ func (sh *streamHandler) handleStream(srv interface{}, stream grpc.ServerStream)
 
 	proxyDone := make(chan *status.Status)
 	go func() {
-		s := sh.handleRequest(stream, req)
+		s := p.handleRequest(stream, req)
 		proxyDone <- s
 	}()
 
@@ -69,7 +58,7 @@ func (sh *streamHandler) handleStream(srv interface{}, stream grpc.ServerStream)
 	return status.Error(result.Code(), result.Message())
 }
 
-func (sh *streamHandler) handleRequest(stream grpc.ServerStream, req *request) *status.Status {
+func (p *Proxy) handleRequest(stream grpc.ServerStream, req *request) *status.Status {
 	if err := readRequest(stream, req); err != nil {
 		req.errorLogger.Error("failed to read request from request:" + err.Error())
 		return status.Newf(codes.Internal, "failed to read request from request: %v", err)
@@ -85,9 +74,13 @@ func (sh *streamHandler) handleRequest(stream grpc.ServerStream, req *request) *
 	var err error
 	retrying := true
 	for retrying {
-		fcgiReq := req.toFcgiRequest(sh.fcgiOptions)
+		fcgiReq := req.toFcgiRequest(p.opt.Fcgi)
+		if p.opt.Log.AccessLogTrace {
+			fcgiReq = fcgiReq.WithTrace()
+		}
+
 		round := req.rotateRound(fcgiReq)
-		resp, retrying, err = sh.fcgiClient.RoundTrip(fcgiReq)
+		resp, retrying, err = p.fcgiClient.RoundTrip(fcgiReq)
 		round.resp = resp
 		round.err = err
 	}
@@ -98,10 +91,10 @@ func (sh *streamHandler) handleRequest(stream grpc.ServerStream, req *request) *
 	// read information about the final response
 	req.bodyBytesSent = len(resp.Body)
 
-	return sh.sendResponse(stream, resp)
+	return p.sendResponse(stream, resp)
 }
 
-func (sh *streamHandler) sendResponse(stream grpc.ServerStream, resp *fcgi.Response) *status.Status {
+func (p *Proxy) sendResponse(stream grpc.ServerStream, resp *fcgi.Response) *status.Status {
 	// TODO: convert resp code to grpc code
 	statusCode, err := resp.GetStatusCode()
 	if err != nil {
@@ -112,7 +105,7 @@ func (sh *streamHandler) sendResponse(stream grpc.ServerStream, resp *fcgi.Respo
 	}
 
 	responseMetadata := metadata.MD{}
-	for k, v := range sh.filterToGrpcHeaders(resp.Headers) {
+	for k, v := range p.filterToGrpcHeaders(resp.Headers) {
 		responseMetadata[strings.ToLower(k)] = v
 	}
 	if err := stream.SendHeader(responseMetadata); err != nil {
@@ -129,8 +122,7 @@ func (sh *streamHandler) sendResponse(stream grpc.ServerStream, resp *fcgi.Respo
 	return status.Newf(codes.OK, "")
 }
 
-func (sh *streamHandler) filterToGrpcHeaders(fcgiHeaders map[string][]string) map[string][]string {
+func (p *Proxy) filterToGrpcHeaders(fcgiHeaders map[string][]string) map[string][]string {
 	// this probably need to be munged?
 	return fcgiHeaders
 }
-
